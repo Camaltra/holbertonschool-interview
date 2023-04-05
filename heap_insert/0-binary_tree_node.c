9df8ed17ccf7242d3fb90f444c338ccf5e9dc277
Camaltra <mickael.boillaud@gmail.com>
2023-04-05 15:53:21 +0200

    Betty -- This is the real unfair

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -11,25 +11,25 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-  binary_tree_t *node_to_insert = NULL;
+	binary_tree_t *node_to_insert = NULL;
 
-  node_to_insert = malloc(sizeof(binary_tree_t));
-  if (!node_to_insert)
-    return NULL;
+	node_to_insert = malloc(sizeof(binary_tree_t));
+	if (!node_to_insert)
+		return (NULL);
 
-  node_to_insert->n = value;
-  if (parent)
-  {
-    node_to_insert->parent = parent;
-    if (!parent->left)
-      parent->left = node_to_insert;
-    else
-      parent->right = node_to_insert;
-  }
-  else
-    node_to_insert->parent = NULL;
-  node_to_insert->left = NULL;
-  node_to_insert->right = NULL;
+	node_to_insert->n = value;
+	if (parent)
+	{
+		node_to_insert->parent = parent;
+		if (!parent->left)
+			parent->left = node_to_insert;
+		else
+			parent->right = node_to_insert;
+	}
+	else
+		node_to_insert->parent = NULL;
+	node_to_insert->left = NULL;
+	node_to_insert->right = NULL;
 
-  return (node_to_insert);
+	return (node_to_insert);
 }
