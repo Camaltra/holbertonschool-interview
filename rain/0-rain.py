7eefef8ea8fd14460744e973ba8c5e226204fd0a
Camaltra <mickael.boillaud@gmail.com>
2024-01-05 16:53:59 +0100

    Checks -- PEP8

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -3,6 +3,7 @@
 Useless comment
 """
 
+
 def rain(walls):
     """
     Useless comment
@@ -12,8 +13,9 @@ def rain(walls):
     if len(walls) == 0:
         return 0
 
-    left_max = [walls[0]] + [max(walls[:i + 1]) for i in range(1, len(walls))]
-    right_max = [max(walls[i:]) for i in range(len(walls) - 2, -1, -1)][::-1] + [walls[-1]]
+    left_max = [walls[0]] + [max(walls[: i + 1]) for i in range(1, len(walls))]
+    right_max = [max(walls[i:]) for i in range(len(walls) - 2, -1, -1)][::-1]
+    right_max += [walls[-1]]
 
     water = 0
     for i, height in enumerate(walls):
