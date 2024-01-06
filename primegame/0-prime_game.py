92d0697594f1af26c57c204d7b898e47abe4bf4c
Camaltra <mickael.boillaud@gmail.com>
2024-01-06 22:41:25 +0100

    Tasks -- Add

--- primegame/0-prime_game.py
+++ primegame/0-prime_game.py
@@ -1,76 +1,34 @@
 #!/usr/bin/python3
-
-"""
-Useless
-"""
-
-
-def is_prime(x):
-    """
-    Useless comment
-    """
-    for i in range(2, x):
-        if (x % i) == 0:
-            return False
-    return True
-
-
-def get_next_prime(numbers):
-    """
-    Useless comment
-    """
-    for num in numbers:
-        if is_prime(num) and num != 1:
-            return num
-    return None
-
-
-def compute_new_list(numbers, last_prime):
-    """
-    Compute the new list by delete all the multiple of given prime num
-    :param numbers: Last list
-    :param last_prime: Last prime
-    :return: The new list
-    """
-    new_numbers = []
-    for i in range(len(numbers)):
-        if numbers[i] % last_prime != 0:
-            new_numbers.append(numbers[i])
-    return new_numbers
+""" Module for Prime Game """
 
 
 def isWinner(x, nums):
-    """
-    Determine the winner
-    :param x: The number of game to play
-    :param nums: The num limit for each game
-    :return:
-    """
-    first_player_wins = 0
-    second_player_wins = 0
-
-    if x <= 0 or x > len(nums):
+    """Solves Prime Game"""
+    if not nums or x < 1:
         return None
-
-    for ix_party in range(x):
-        if nums[ix_party] == 0:
+    n = max(nums)
+    sieve = [True for _ in range(max(n + 1, 2))]
+    for i in range(2, int(pow(n, 0.5)) + 1):
+        if not sieve[i]:
             continue
-        numbers = [i for i in range(1, nums[ix_party] + 1)]
-        steps = 1
-        while True:
-            num = get_next_prime(numbers)
-            if num is None:
-                if steps % 2 != 0:
-                    second_player_wins += 1
-                else:
-                    first_player_wins += 1
-                break
-            numbers = compute_new_list(numbers, num)
-            steps += 1
+        for j in range(i*i, n + 1, i):
+            sieve[j] = False
 
-    if first_player_wins > second_player_wins:
-        return "Maria"
-    if second_player_wins > first_player_wins:
-        return "Ben"
-    return None
+    sieve[0] = sieve[1] = False
+    c = 0
+    for i in range(len(sieve)):
+        if sieve[i]:
+            c += 1
+        sieve[i] = c
 
+    winner = ''
+    player1 = 0
+    for n in nums:
+        player1 += sieve[n] % 2 == 1
+    if player1 * 2 == len(nums):
+        winner = None
+    if player1 * 2 > len(nums):
+        winner = "Maria"
+    else:
+        winner = "Ben"
+    return winner
